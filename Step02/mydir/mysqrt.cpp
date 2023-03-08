06531bf02a4d3106156664de99c31082773f348c
zhangtaoshan <zhangtaoshan@sensetime.com>
2023-03-08 11:07:18 +0800

    update Step01&&Step02

---
+++ Step02/mydir/mysqrt.cpp
@@ -0,0 +1,31 @@
+#include "mysqrt.h"
+#include <iostream>
+
+namespace mathfunctions
+{
+namespace detail
+{
+double mysqrt(double x)
+{
+    if (x <= 0)
+    {
+	return 0;
+    }
+
+    double result = x;
+
+    for (int i = 0; i < 10; ++i)
+    {
+	if (result <= 0)
+	{
+	    result = 0.1;
+	}
+	double delta = x - (result * result);
+	result = result + 0.5 * delta / result;
+	std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
+    }
+    return result;
+}
+}
+}
+
